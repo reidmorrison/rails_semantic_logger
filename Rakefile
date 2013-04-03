325f3532f98a06abc2f674f69c49a5287d2c352c
Reid Morrison <reidmo@gmail.com>
2013-04-03 18:27:26 -0400

    Upgrade to SemanticLogger V2.1

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@ lib = File.expand_path('../lib/', __FILE__)
 $:.unshift lib unless $:.include?(lib)
 
 require 'rubygems'
+require 'rubygems/package'
 require 'rake/clean'
 require 'rake/testtask'
 require 'date'
@@ -19,11 +20,12 @@ task :gem  do |t|
     spec.date        = Date.today.to_s
     spec.summary     = "Improved logging for Ruby on Rails"
     spec.description = "Replaces the default Rails logger with SemanticLogger"
-    spec.files       = FileList["./**/*"].exclude(/.gem$/, /.log$/,/^nbproject/).map{|f| f.sub(/^\.\//, '')}
+    spec.files       = FileList["./**/*"].exclude(/\.gem$/, /\.log$/,/nbproject/).map{|f| f.sub(/^\.\//, '')}
+    spec.license     = "Apache License V2.0"
     spec.has_rdoc    = true
-    spec.add_dependency 'semantic_logger', '>= 2'
-    spec.add_dependency 'rails', '>= 2'
+    spec.add_dependency 'semantic_logger', '>= 2.1'
+    #spec.add_dependency 'rails', '>= 2'
   end
-  Gem::Builder.new(gemspec).build
+  Gem::Package.build gemspec
 end
 
--- lib/rails_semantic_logger/railtie.rb
+++ lib/rails_semantic_logger/railtie.rb
@@ -9,7 +9,7 @@ module RailsSemanticLogger #:nodoc:
     #   Claritybase::Application.configure do
     #     # Add the MongoDB logger appender only once Rails is initialized
     #     config.after_initialize do
-    #       config.semantic_logger.appenders << SemanticLogger::Appender::Mongo.new(
+    #       config.semantic_logger.add_appender SemanticLogger::Appender::Mongo.new(
     #         :db => Mongo::Connection.new['development_development']
     #        )
     #     end
@@ -20,15 +20,13 @@ module RailsSemanticLogger #:nodoc:
     # insert itself above the configured rails logger to add support for its
     # additional features
     #
-    # Also, if Mongoid is installed it will automatically start logging to Mongoid
-    #
     # Loaded after Rails logging is initialized since SemanticLogger will continue
     # to forward logging to the Rails Logger
     initializer :initialize_semantic_logger, :before => :initialize_logger do
       config = Rails.application.config
 
       # Set the default log level based on the Rails config
-      SemanticLogger::Logger.default_level = config.log_level
+      SemanticLogger.default_level = config.log_level
 
       # Existing loggers are ignored because servers like trinidad supply their
       # own file loggers which would result in duplicate logging to the same log file
@@ -42,20 +40,20 @@ module RailsSemanticLogger #:nodoc:
         # Set internal logger to log to file only, in case another appender
         # experiences errors during writes
         appender = SemanticLogger::Appender::File.new(path, config.log_level)
-        appender.name = "SemanticLogger::Logger"
+        appender.name = "SemanticLogger"
         SemanticLogger::Logger.logger = appender
 
         # Add the log file to the list of appenders
-        SemanticLogger::Logger.appenders << SemanticLogger::Appender::File.new(path)
-        SemanticLogger::Logger.new(Rails)
+        SemanticLogger.add_appender(path)
+        SemanticLogger[Rails]
       rescue StandardError
         # If not able to log to file, log to standard error with warning level only
-        SemanticLogger::Logger.default_level = :warn
+        SemanticLogger.default_level = :warn
 
         SemanticLogger::Logger.logger = SemanticLogger::Appender::File.new(STDERR)
-        SemanticLogger::Logger.appenders << SemanticLogger::Appender::File.new(STDERR)
+        SemanticLogger.add_appender(STDERR)
 
-        logger = SemanticLogger::Logger.new(Rails)
+        logger = SemanticLogger[Rails]
         logger.warn(
           "Rails Error: Unable to access log file. Please ensure that #{path} exists and is chmod 0666. " +
             "The log level has been raised to WARN and the output directed to STDERR until the problem is fixed."
@@ -64,9 +62,9 @@ module RailsSemanticLogger #:nodoc:
       end
 
       # Replace the default Rails loggers
-      ActiveSupport.on_load(:active_record)     { self.logger = SemanticLogger::Logger.new('ActiveRecord') }
-      ActiveSupport.on_load(:action_controller) { self.logger = SemanticLogger::Logger.new('ActionController') }
-      ActiveSupport.on_load(:action_mailer)     { self.logger = SemanticLogger::Logger.new('ActionMailer') }
+      ActiveSupport.on_load(:active_record)     { self.logger = SemanticLogger['ActiveRecord'] }
+      ActiveSupport.on_load(:action_controller) { self.logger = SemanticLogger['ActionController'] }
+      ActiveSupport.on_load(:action_mailer)     { self.logger = SemanticLogger['ActionMailer'] }
     end
 
   end

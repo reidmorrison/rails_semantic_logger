57419d7f78b69d99320076f2abdd71e78c8518b8
Reid Morrison <reidmo@gmail.com>
2016-02-29 11:36:50 -0500

    Rails 5 Beta 3 renamed Active job method #tag to #tag_logger

--- lib/rails_semantic_logger/extensions/action_cable/tagged_logger_proxy.rb
+++ lib/rails_semantic_logger/extensions/action_cable/tagged_logger_proxy.rb
@@ -3,6 +3,12 @@ ActionCable::Connection::TaggedLoggerProxy
 module ActionCable
   module Connection
     class TaggedLoggerProxy
+      # As of Rails 5 Beta 3
+      def tag_logger(*tags, &block)
+        logger.tagged(*tags, &block)
+      end
+
+      # Rails 5 Beta 1,2. TODO: Remove once Rails 5 is GA
       def tag(logger, &block)
         current_tags = tags - logger.tags
         logger.tagged(*current_tags, &block)

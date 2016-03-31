7538822f9a123cb2e16fb285caa9970ddb52876c
Reid Morrison <reidmo@gmail.com>
2016-03-31 14:55:20 -0400

    Fix travis exclusion list

--- test/rails_test.rb
+++ test/rails_test.rb
@@ -3,8 +3,13 @@ require_relative 'test_helper'
 class RailsTest < Minitest::Test
   describe 'Rails' do
     describe '.logger' do
-      it 'sets the Rails logger' do
+      it 'replaces the Rails logger' do
         assert_kind_of SemanticLogger::Logger, Rails.logger
+        assert_kind_of SemanticLogger::Formatters::Color, Rails.logger.formatter
+      end
+
+      it 'uses the colorized formatter' do
+        assert_kind_of SemanticLogger::Formatters::Color, Rails.logger.formatter
       end
     end
   end

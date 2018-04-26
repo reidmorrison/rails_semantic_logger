7b07eb6e89dbd1ea8b56e4b1b4308de4fa284c41
Reid Morrison <reidmo@gmail.com>
2018-04-26 11:10:41 -0400

    Fix when engine does not use assets

--- lib/rails_semantic_logger/engine.rb
+++ lib/rails_semantic_logger/engine.rb
@@ -176,7 +176,7 @@ module RailsSemanticLogger
 
     # Before any initializers run, but after the gems have been loaded
     config.before_initialize do
-      if config.assets && defined?(Rails::Rack::Logger) && config.rails_semantic_logger.semantic
+      if config.respond_to?(:assets) && defined?(Rails::Rack::Logger) && config.rails_semantic_logger.semantic
         config.rails_semantic_logger.quiet_assets = true if config.assets.quiet
 
         # Otherwise Sprockets can't find the Rails::Rack::Logger middleware

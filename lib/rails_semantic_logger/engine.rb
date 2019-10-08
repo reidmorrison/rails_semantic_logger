21414241f8778934cae6a365d12abda25e9bd562
Bob van der Linden <bobvanderlinden@gmail.com>
2019-10-08 14:59:31 +0200

    Use Sidekiq.logger when Sidekiq::Logging is unavailable (#95)
    * use Sidekiq.logger when Sidekiq::Logging is unavailable
    
    * always use Sidekiq.logger

--- lib/rails_semantic_logger/engine.rb
+++ lib/rails_semantic_logger/engine.rb
@@ -108,7 +108,7 @@ module RailsSemanticLogger
       Resque.logger           = SemanticLogger[Resque] if defined?(Resque) && Resque.respond_to?(:logger)
 
       # Replace the Sidekiq logger
-      Sidekiq::Logging.logger = SemanticLogger[Sidekiq] if defined?(Sidekiq)
+      Sidekiq.logger = SemanticLogger[Sidekiq] if defined?(Sidekiq)
 
       # Replace the Sidetiq logger
       Sidetiq.logger          = SemanticLogger[Sidetiq] if defined?(Sidetiq)

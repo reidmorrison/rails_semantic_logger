f89944e0f76f97eae95d1985617d774d65157ca2
Arjan Diepenbroek <arjan0307@gmail.com>
2021-04-07 11:37:15 +0200

    Add whether a sql statement came from the query cache to ActiveRecord log statements

--- lib/rails_semantic_logger/active_record/log_subscriber.rb
+++ lib/rails_semantic_logger/active_record/log_subscriber.rb
@@ -32,6 +32,7 @@ module RailsSemanticLogger
         log_payload         = {sql: payload[:sql]}
         log_payload[:binds] = bind_values(payload) unless (payload[:binds] || []).empty?
         log_payload[:allocations] = event.allocations if event.respond_to?(:allocations)
+        log_payload[:cached] = event.payload[:cached]
 
         log = {
           message:  name,

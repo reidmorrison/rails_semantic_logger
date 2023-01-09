31f0c91f1944a68090cb8dcd4e1486a72151fdde
Kacper Madej <kacperoza@gmail.com>
2023-01-09 12:40:48 +0100

    Do not process ActionController if params is not a Hash

---
+++ test/action_controller_test.rb
@@ -0,0 +1,27 @@
+require_relative "test_helper"
+
+class ActionControllerTest < Minitest::Test
+  describe "RailsSemanticLogger::ActionController::LogSubscriber" do
+    let(:subscriber) { RailsSemanticLogger::ActionController::LogSubscriber.new }
+
+    describe "#process_action" do
+      it "does not process if params is not a Hash nor an instance of ActionController::Parameters" do
+        event = ActiveSupport::Notifications::Event.new(
+          "start_processing.action_controller",
+          5.seconds.ago,
+          Time.zone.now,
+          SecureRandom.uuid,
+          {
+            payload: "{}"
+          }
+        )
+
+        messages = semantic_logger_events do
+          subscriber.process_action(event)
+        end
+
+        assert_equal 1, messages.count, messages
+      end
+    end
+  end
+end

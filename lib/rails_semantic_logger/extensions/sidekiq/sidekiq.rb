34e58370b850837f909446728b5e402224834e1c
Reid Morrison <reidmo@gmail.com>
2024-06-27 11:48:28 -0400

    Initial Sidekiq 7 support

--- lib/rails_semantic_logger/extensions/sidekiq/sidekiq.rb
+++ lib/rails_semantic_logger/extensions/sidekiq/sidekiq.rb
@@ -15,8 +15,12 @@ elsif Sidekiq::VERSION.to_i == 5
   require "sidekiq/job_logger"
   require "sidekiq/logging"
   require "sidekiq/worker"
-elsif Sidekiq::VERSION.to_i > 5
+elsif Sidekiq::VERSION.to_i == 6
   require "sidekiq/exception_handler"
+  require "sidekiq/job_logger"
+  require "sidekiq/worker"
+else
+  require "sidekiq/config"
   require "sidekiq/job_logger"
   require "sidekiq/worker"
 end
@@ -26,15 +30,15 @@ module Sidekiq
     # Let Semantic Logger handle duration logging
     class JobLogger
       def call(item, queue)
-        klass = item["wrapped"] || item["class"]
+        klass  = item["wrapped"] || item["class"]
         metric = "Sidekiq/#{klass}/perform" if klass
         logger = klass ? SemanticLogger[klass] : Sidekiq.logger
         logger.info("Start #perform")
         logger.measure_info(
           "Completed #perform",
           on_exception_level: :error,
-          log_exception: :full,
-          metric: metric
+          log_exception:      :full,
+          metric:             metric
         ) do
           yield
         end
@@ -49,6 +53,16 @@ module Sidekiq
         else
           SemanticLogger.tagged(job_hash_context(job_hash), &block)
         end
+      end
+
+      def job_hash_context(job_hash)
+        h        = {
+          class: job_hash["display_class"] || job_hash["wrapped"] || job_hash["class"],
+          jid:   job_hash["jid"]
+        }
+        h[:bid]  = job_hash["bid"] if job_hash["bid"]
+        h[:tags] = job_hash["tags"] if job_hash["tags"]
+        h
       end
     end
   end
@@ -67,20 +81,31 @@ module Sidekiq
         event
       end
     end
-  end
 
-  # Exception is already logged by Semantic Logger during the perform call
-  module ExceptionHandler
-    class Logger
-      def call(ex, ctx)
-        Sidekiq.logger.warn(ctx) if !ctx.empty?
+    # Exception is already logged by Semantic Logger during the perform call
+    module ExceptionHandler
+      class Logger
+        def call(ex, ctx)
+          Sidekiq.logger.warn(ctx) if !ctx.empty?
+        end
       end
     end
   end
 
-  module Worker
-    # Logging within each worker should use its own logger
-    if Sidekiq::VERSION.to_i == 4
+  if Sidekiq::VERSION.to_i >= 7
+    module Config
+      remove_const :ERROR_HANDLER
+
+      # Exception is already logged by Semantic Logger during the perform call
+      ERROR_HANDLER = ->(ex, ctx, cfg = Sidekiq.default_configuration) {
+        cfg.logger.warn(ctx) unless ctx.empty?
+      }
+    end
+  end
+
+  # Logging within each worker should use its own logger
+  if Sidekiq::VERSION.to_i == 4
+    module Worker
       def self.included(base)
         raise ArgumentError, "You cannot include Sidekiq::Worker in an ActiveJob: #{base.name}" if base.ancestors.any? { |c| c.name == "ActiveJob::Base" }
 
@@ -90,7 +115,9 @@ module Sidekiq
         base.class_attribute :sidekiq_retry_in_block
         base.class_attribute :sidekiq_retries_exhausted_block
       end
-    elsif Sidekiq::VERSION.to_i == 5
+    end
+  elsif Sidekiq::VERSION.to_i == 5
+    module Worker
       def self.included(base)
         raise ArgumentError, "You cannot include Sidekiq::Worker in an ActiveJob: #{base.name}" if base.ancestors.any? { |c| c.name == "ActiveJob::Base" }
 
@@ -100,9 +127,21 @@ module Sidekiq
         base.sidekiq_class_attribute :sidekiq_retry_in_block
         base.sidekiq_class_attribute :sidekiq_retries_exhausted_block
       end
-    elsif Sidekiq::VERSION.to_i > 5
+    end
+  elsif Sidekiq::VERSION.to_i == 6
+    module Worker
       def self.included(base)
         raise ArgumentError, "Sidekiq::Worker cannot be included in an ActiveJob: #{base.name}" if base.ancestors.any? { |c| c.name == "ActiveJob::Base" }
+
+        base.include(Options)
+        base.extend(ClassMethods)
+        base.include(SemanticLogger::Loggable)
+      end
+    end
+  else
+    module Job
+      def self.included(base)
+        raise ArgumentError, "Sidekiq::Job cannot be included in an ActiveJob: #{base.name}" if base.ancestors.any? { |c| c.name == "ActiveJob::Base" }
 
         base.include(Options)
         base.extend(ClassMethods)
@@ -130,8 +169,8 @@ module Sidekiq
             worker.logger.measure_info(
               "Completed #perform",
               on_exception_level: :error,
-              log_exception: :full,
-              metric: "Sidekiq/#{worker.class.name}/perform"
+              log_exception:      :full,
+              metric:             "Sidekiq/#{worker.class.name}/perform"
             ) do
               yield
             end

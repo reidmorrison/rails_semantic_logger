0e031871d6bf3f60ae998f0a627772838b263beb
Karl Entwistle <me@karlentwistle.com>
2025-06-13 10:02:19 +0100

    https://github.com/reidmorrison/rails_semantic_logger/pull/254\#issuecomment-2638306606

--- lib/rails_semantic_logger/engine.rb
+++ lib/rails_semantic_logger/engine.rb
@@ -266,8 +266,10 @@ module RailsSemanticLogger
       # Re-open appenders after Spring has forked a process
       Spring.after_fork { |_job| ::SemanticLogger.reopen } if defined?(Spring.after_fork)
 
-      # Re-open appenders after SolidQueue has forked a worker
+      # Re-open appenders after SolidQueue worker/dispatcher/scheduler has finished booting
       SolidQueue.on_worker_start { ::SemanticLogger.reopen } if defined?(SolidQueue.on_worker_start)
+      SolidQueue.on_dispatcher_start { ::SemanticLogger.reopen } if defined?(SolidQueue.on_dispatcher_start)
+      SolidQueue.on_scheduler_start { ::SemanticLogger.reopen } if defined?(SolidQueue.on_scheduler_start)
 
       console do |_app|
         # Don't use a background thread for logging

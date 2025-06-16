f0345084c147a6945b3368518c4f9720808a361a
Karl Entwistle <me@karlentwistle.com>
2025-06-16 16:31:05 +0100

    add SolidQueue.on_start

--- lib/rails_semantic_logger/engine.rb
+++ lib/rails_semantic_logger/engine.rb
@@ -267,6 +267,7 @@ module RailsSemanticLogger
       Spring.after_fork { |_job| ::SemanticLogger.reopen } if defined?(Spring.after_fork)
 
       # Re-open appenders after SolidQueue worker/dispatcher/scheduler has finished booting
+      SolidQueue.on_start { ::SemanticLogger.reopen } if defined?(SolidQueue.on_start)
       SolidQueue.on_worker_start { ::SemanticLogger.reopen } if defined?(SolidQueue.on_worker_start)
       SolidQueue.on_dispatcher_start { ::SemanticLogger.reopen } if defined?(SolidQueue.on_dispatcher_start)
       SolidQueue.on_scheduler_start { ::SemanticLogger.reopen } if defined?(SolidQueue.on_scheduler_start)

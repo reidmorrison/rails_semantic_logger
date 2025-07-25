73a84951c0221703c3efcb57898e38f43ce88a4c
Max Jacobson <max@hardscrabble.net>
2025-07-25 13:44:46 -0400

    Respect action_dispatch.log_rescued_responses
    This is a new configuration option in Rails 7 which defaults to true.
    When an app sets this to false, this method does not log anything at all
    for rescued responses.
    
    Because rails_semantic_logger monkey patches
    ActionDispatch::DebugExceptions and overrides this method, this
    configuration option does not have any effect. Let's correct that.
    
    More context on this configuration option:
    
    - PR which introduced it https://github.com/rails/rails/pull/42592
    - Evidence that this implementation should work fine in Rails 7.1+
      https://github.com/rails/rails/blob/v7.1.0/actionpack/lib/action_dispatch/middleware/debug_exceptions.rb#L137
    
    I also noticed that rails_semantic_logger is not respecting the
    action_dispatch.debug_exception_log_level configuration, and it makes
    its own decisions about what is an appropriate level. I updated this to
    also follow that configuration.
    
    This is also how it works in Rails 7.1+:
    https://github.com/rails/rails/blob/v7.1.0/actionpack/lib/action_dispatch/middleware/debug_exceptions.rb#L154

--- lib/rails_semantic_logger/extensions/action_dispatch/debug_exceptions.rb
+++ lib/rails_semantic_logger/extensions/action_dispatch/debug_exceptions.rb
@@ -7,9 +7,11 @@ module ActionDispatch
 
     undef_method :log_error
     if (Rails::VERSION::MAJOR == 7 && Rails::VERSION::MINOR >= 1) || Rails::VERSION::MAJOR > 7
-      def log_error(_request, wrapper)
+      def log_error(request, wrapper)
         Rails.application.deprecators.silence do
-          level = wrapper.respond_to?(:rescue_response?) && wrapper.rescue_response? ? :debug : :fatal
+          return if !log_rescued_responses?(request) && wrapper.rescue_response?
+
+          level = request.get_header("action_dispatch.debug_exception_log_level")
           ActionController::Base.logger.log(level, wrapper.exception)
         end
       end

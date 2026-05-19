2db1403f9293cbc73d0c8b0d0089ce6e3a1446f1
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2026-05-19 23:38:49 +0100

    Fix ActionCable TaggedLoggerProxy when logger lacks tags.
    Avoid calling logger.tags unconditionally so TaggedLoggerProxy works with BroadcastLogger-style loggers, and add a regression test for loggers that only implement tagged.

--- lib/rails_semantic_logger/extensions/action_cable/tagged_logger_proxy.rb
+++ lib/rails_semantic_logger/extensions/action_cable/tagged_logger_proxy.rb
@@ -4,7 +4,7 @@ module ActionCable
   module Connection
     class TaggedLoggerProxy
       def tag(logger, &block)
-        current_tags = tags - logger.tags
+        current_tags = tags - (logger.respond_to?(:tags) ? Array(logger.tags) : [])
         logger.tagged(*current_tags, &block)
       end
     end
